b6ff187854b05be5768686dbd6db9026d73d159e
francojnieva <franco97nieva@hotmail.com>
2023-07-07 20:26:54 -0300

    :art: add content to project cards

--- js/projects.js
+++ js/projects.js
@@ -3,28 +3,28 @@ const projects = [
         img: "../img/portafolio-.png",
         alt:"Imagen de proyecto portafolio",
         title:"Portafolio",
-        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit rem repellendus nesciunt eos blanditiis quidem deserunt ducimus mollitia nihil expedita provident minus aliquid laborum saepe reprehenderit delectus doloremque, ea sint.",
+        description:"Es una página web desarrollada utilizando HTML, CSS y JavaScript. Este portafolio digital brinda una presentación creativa y visualmente atractiva de los proyectos y habilidades. Con un diseño responsive y fácil navegación, proporciona información personal y un formulario para contacto profesional",
         repo:"https://github.com/francojnieva/Portafolio.git",
     },
     {
         img: "../img/tecnoStore-.png",
         alt:"Imagen de proyecto e-commerce TecnoStore",
         title:"E-commerce",
-        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit rem repellendus nesciunt eos blanditiis quidem deserunt ducimus mollitia nihil expedita provident minus aliquid laborum saepe reprehenderit delectus doloremque, ea sint.",
+        description:"Es un sitio web desarrollado en HTML, CSS, Bootstrap y JavaScript. Fue llevado a cabo por un equipo de 3 integrantes mientras aprendíamos JavaScript en Rolling Code School. El objetivo principal del ecommerce fue lograr la persistencia de datos, permitiendo a los usuarios agregar productos al carrito",
         repo:"https://github.com/francojnieva/RCS-TecnoStore.git",
     },
     {
         img: "img/educandoParaElFuturo-.png",
         alt:"Imagen de proyecto Educando para el futuro",
         title:"Educando para el futuro",
-        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit rem repellendus nesciunt eos blanditiis quidem deserunt ducimus mollitia nihil expedita provident minus aliquid laborum saepe reprehenderit delectus doloremque, ea sint.",
+        description:"Es un sitio web estático desarrollado utilizando HTML, CSS y Bootstrap. Es un proyecto realizado por 4 integrantes en el curso de Rolling Code School. El objetivo principal fue maquetar y estilizar un sitio web, trabajando de manera colaborativa a través de GitHub y gestionando las tareas por medio de Trello.",
         repo:"https://github.com/MigPalomares/RCS-Proyect-Education.git",
     },
     {
         img: "../img/lista-de-tareas.png",
         alt:"Imagen de proyecto e-commerce TecnoStore",
         title:"Lista de Tareas",
-        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit rem repellendus nesciunt eos blanditiis quidem deserunt ducimus mollitia nihil expedita provident minus aliquid laborum saepe reprehenderit delectus doloremque, ea sint.",
+        description:"Es una página web creada con HTML, CSS, Bootstrap y JavaScript. Con un elegante diseño en tonos oscuros, esta lista de tareas ofrece una experiencia responsive y amigable para el usuario. Permite crear, marcar tareas completadas de manera sencilla y borrarlas",
         repo:"https://github.com/francojnieva/To-Do-List.git",
     }
 ]
